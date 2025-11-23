ac04ea905c7e5736785bbe0c37893f5b0d7b9257
David Front <dfront@gmail.com>
2025-11-23 10:18:28 +0200

    chore: remove archived infra tooling

--- terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-# Defines the outputs from our Terraform configuration.
-
-output "project_number" {
-  description = "The number of the created Google Cloud project."
-  value       = google_project.project.number
-}
